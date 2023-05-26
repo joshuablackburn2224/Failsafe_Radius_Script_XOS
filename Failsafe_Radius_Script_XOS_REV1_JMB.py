e8b7c64e4e1f2c5d20fee3eedfa4aaed85629fa7
Joshua Blackburn <113396594+joshuablackburn2224@users.noreply.github.com>
2023-05-26 11:03:20 -0400

    added spaces

--- Failsafe_Radius_Script_XOS_REV1_JMB.py
+++ Failsafe_Radius_Script_XOS_REV1_JMB.py
@@ -3,7 +3,7 @@ from xmclib import emc_vars
 from xmclib import cli
 from device import api
 from device.deviceutils import DeviceUtils
-# emc_vars['device_ip']
+# emc_vars['deviceIP']
 
 emc_cli.send("configure failsafe-account", False)
 emc_cli.send("justin2023", False)
@@ -15,7 +15,7 @@ emc_cli.send("y", False)
 
 #changed logic to work with exsh
 device_IP = emc_vars['deviceIP']
-command1 = 'configure radius mgmt-access primary server 10.60.7.103 1812 client-ip' + device_IP + 'vr VR-Default'
+command1 = 'configure radius mgmt-access primary server 10.60.7.103 1812 client-ip ' + device_IP + ' vr VR-Default'
 emc_cli.send( command1 , False)
 emc_cli.send('configure radius mgmt-access primary shared-secret a7z3436f2nteyf3u4n8frwpucsakk6', False)
 emc_cli.send('enable radius', False)
