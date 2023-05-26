f53fcb229169ff8b7380bc29aa33000122d29425
Joshua Blackburn <113396594+joshuablackburn2224@users.noreply.github.com>
2023-05-26 10:16:40 -0400

    First Functional Trial

--- Failsafe_Radius_Script_XOS_REV0_JMB.py
+++ Failsafe_Radius_Script_XOS_REV0_JMB.py
@@ -4,7 +4,9 @@ from xmclib import emc_vars
 from xmclib import cli
 from device import api
 from device.deviceutils import DeviceUtils
+import re
 
+pattern = re.compile("(\d){1,3}.(\d){1,3}.(\d){1,3}.(\d){1,3}")
 emc_cli.send("configure failsafe-account", False)
 emc_cli.send("justin2023", False)
 emc_cli.send("warren17", False)
@@ -13,13 +15,13 @@ emc_cli.send("configure failsafe-account permit all", False)
 emc_cli.send("save", False)
 emc_cli.send("y", False)
 
-device_IP = emc_cli.send('show WCSmgmt | include Primary', True)
-print (device_IP)
-"""
-Grab the IP $.$.$.$
-emc_cli.send('configure radius mgmt-access primary server 10.60.7.103 1812 client-ip $.$.$.$ vr VR-Default', False)
+device_IP_String = emc_cli.send('show WCSmgmt | include Primary', True)
+print (device_IP_String)
+IP_List = pattern.findall(device_IP_String)
+device_IP_Final = IP_List[0]
+command1 = 'configure radius mgmt-access primary server 10.60.7.103 1812 client-ip' + device_IP_Final + 'vr VR-Default'
+emc_cli.send( command1 , False)
 emc_cli.send('configure radius mgmt-access primary shared-secret a7z3436f2nteyf3u4n8frwpucsakk6', False)
 emc_cli.send('enable radius', False)
 emc_cli.send('save', False)
 emc_cli.send('y', False)
-"""
