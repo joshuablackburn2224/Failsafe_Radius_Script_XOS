322c83df7f994eb281e40c7a27f8804246b8e064
Joshua Blackburn <113396594+joshuablackburn2224@users.noreply.github.com>
2023-05-26 10:55:12 -0400

    found variable for device IP
    emc_vars['device_ip']

--- Failsafe_Radius_Script_XOS_REV1_JMB.py
+++ Failsafe_Radius_Script_XOS_REV1_JMB.py
@@ -4,13 +4,7 @@ from xmclib import emc_vars
 from xmclib import cli
 from device import api
 from device.deviceutils import DeviceUtils
-import re
-
-def exosCmd(cmd):
-        print cmd
-        result = exsh.clicmd(cmd, True)
-        print(str(result))
-        return result
+# emc_vars['device_ip']
 
 pattern = re.compile("(\d){1,3}.(\d){1,3}.(\d){1,3}.(\d){1,3}")
 emc_cli.send("configure failsafe-account", False)
@@ -22,11 +16,8 @@ emc_cli.send("save", False)
 emc_cli.send("y", False)
 
 #changed logic to work with exsh
-device_IP_String = exosCmd('show WCSmgmt | include Primary')
-print (device_IP_String)
-IP_List = pattern.findall(device_IP_String)
-device_IP_Final = IP_List[0]
-command1 = 'configure radius mgmt-access primary server 10.60.7.103 1812 client-ip' + device_IP_Final + 'vr VR-Default'
+device_IP = emc_vars['device_ip']
+command1 = 'configure radius mgmt-access primary server 10.60.7.103 1812 client-ip' + device_IP + 'vr VR-Default'
 emc_cli.send( command1 , False)
 emc_cli.send('configure radius mgmt-access primary shared-secret a7z3436f2nteyf3u4n8frwpucsakk6', False)
 emc_cli.send('enable radius', False)
